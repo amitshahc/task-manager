5af6c6d04f67d2aed77c4bc97dcc396a03a8fdb6
amitshahc <amitshahc@hotmail.com>
2025-01-22 22:30:03 +0530

    migration and controllers

--- Modules/Tasks/app/Http/Controllers/TasksController.php
+++ Modules/Tasks/app/Http/Controllers/TasksController.php
@@ -3,32 +3,65 @@
 namespace Modules\Tasks\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Contracts\Database\Query\Builder;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
+use Modules\Tasks\Repositories\ProjectsRepository;
+use Modules\Tasks\Repositories\TasksRepository;
+use Throwable;
 
 class TasksController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request, ProjectsRepository $repoProjects, TasksRepository $repoTasks)
     {
-        return view('tasks::index');
+
+        $projects = $repoProjects->getUserProjectList(Auth::user());
+
+        $project_id_current = $request->has('project_id_current') ?  $request->get('project_id_current') : $projects->first()->id;
+        
+        $tasks = $repoTasks->getProjectTasks(Auth::user(), $project_id_current);
+        
+        return view('tasks::task-list', ['projects' => $projects, 'tasks' => $tasks, 'project_id_current' => $project_id_current]);
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(Request $request, ProjectsRepository $repoProjects)
     {
-        return view('tasks::create');
+        $project = $repoProjects->getUserProject(Auth::user(), $request->get('project_id_current'));
+        return view('tasks::task-create', ['project' => $project]);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request, TasksRepository $repo)
     {
-        //
+        $validated = $request->validate([
+            'title' => [
+                'required',
+                'max:100',
+                Rule::unique('tasks', 'title')->where(fn(Builder $query) => $query->where('project_id', $request->get('project_id')))
+            ],
+            'description' => ['required', 'max:255'],
+            'project_id' => [
+                'required',
+                Rule::exists('projects', 'id')->where(fn(Builder $query) => $query->where('user_id', Auth::id()))
+            ]
+        ]);
+
+        try {
+            $repo->createTask(Auth::user(), $validated);
+        } catch (Throwable $th) {
+            return redirect()->route('tasks.create', ['project_id' => $request->get('project_id')])->with('error', $th->getMessage());
+        }
+
+        return redirect()->route('tasks.index')->with('success',  __('Task created successfully.'));
     }
 
     /**
