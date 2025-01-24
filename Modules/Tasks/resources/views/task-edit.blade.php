d18be4a589d3f7d7d88b3e656a1e82f55e6bc898
amitshahc <amitshahc@hotmail.com>
2025-01-25 00:05:20 +0530

    Page titles and validation

--- Modules/Tasks/resources/views/task-edit.blade.php
+++ Modules/Tasks/resources/views/task-edit.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="mb-4">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Create New Task') }}
+            {{ __('Edit Task') }}
         </h2>
     </div>
 
